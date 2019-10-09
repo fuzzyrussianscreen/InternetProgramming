4be25d4adcca520207ca4405e5d9e46b7124acbb
OkCur64 OkCur64 <ok.curilo2010@yandex.ru>
2019-10-09 08:23:23 +0400

    lab3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { OrderComponent } from './order/order.component';
+import { ExamplesComponent } from './examples/examples.component';
+import { MainComponent } from './main/main.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'examples',
+    component: ExamplesComponent,
+    data: { title: 'Товары' }
+  },
+  {
+    path: 'order',
+    component: OrderComponent,
+    data: { title: 'Прайс' }
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+    data: { title: 'Контакты' }
+},
+  {
+    path: 'main',
+    component: MainComponent,
+    data: { title: 'Главная' }
+},
+  {
+    path: '',
+    component: MainComponent,
+    data: { title: 'Главная' }
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    OrderComponent,
+    ExamplesComponent,
+    MainComponent
   ],
   imports: [
+      RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
